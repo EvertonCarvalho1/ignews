53d99f069a1a3bf2fc09f96dc657f2bbb182a9f8
Everton Carvalho <82480230+EvertonCarvalho1@users.noreply.github.com>
2023-04-13 15:33:45 -0300

    global app styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
     <>
@@ -9,7 +7,7 @@ export default function Home() {
         <title>Inicio | ig.news</title>
       </Head>
       <h1
-        className={styles.title}
+
       >
         Hello World
       </h1>
