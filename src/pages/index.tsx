5c3506903e9bdc22504329105f039e7d14dcc652
Everton Carvalho <82480230+EvertonCarvalho1@users.noreply.github.com>
2023-04-13 15:19:51 -0300

    dynamic title per page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
+import Head from 'next/head'
+
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1
+        className={styles.title}
+      >
+        Hello World
+      </h1>
+    </>
   )
 }
