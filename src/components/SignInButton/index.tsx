799f48e8c8a6cc01aa41d4069db6bb2c252cf5fd
Everton Carvalho <82480230+EvertonCarvalho1@users.noreply.github.com>
2023-04-14 15:46:11 -0300

    signInButton component

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,40 @@
 import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
+    const isUserLoggedIn = true;
+
     return (
-        <button
-            type="button"
-            className={styles.signInButton}
-        >
-            <FaGithub />
-            Sign in with Github
-        </button>
+        <>
+            {isUserLoggedIn ? (
+
+                <button
+                    type="button"
+                    className={styles.signInButton}
+                >
+                    <FaGithub
+                        color='#04b361'
+                    />
+                    Everton Carvalho
+                    <FiX
+                        color='#737380'
+                        className={styles.closeIcon}
+                    />
+                </button>
+            ) :
+                <button
+                    type="button"
+                    className={styles.signInButton}
+                >
+                    <FaGithub
+                        color='#eba417'
+                    />
+                    Sign in with Github
+                </button>
+
+            }
+        </>
     )
 }
