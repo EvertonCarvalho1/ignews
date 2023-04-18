d7061893909095aea978f99b26cb8748569b4d43
Everton Carvalho <82480230+EvertonCarvalho1@users.noreply.github.com>
2023-04-18 10:56:45 -0300

    authentication with next auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,14 +1,15 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-    const isUserLoggedIn = false;
+    const [session] = useSession();
 
     return (
         <>
-            {isUserLoggedIn ? (
+            {session ? (
 
                 <button
                     type="button"
@@ -27,6 +28,7 @@ export function SignInButton() {
                 <button
                     type="button"
                     className={styles.signInButton}
+                    onClick={() => signIn('github')}
                 >
                     <FaGithub
                         color='#eba417'
