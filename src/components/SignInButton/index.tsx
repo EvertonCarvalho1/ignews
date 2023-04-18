9932a902c03d381a2b9758f3d049a3411fa25f54
Everton Carvalho <82480230+EvertonCarvalho1@users.noreply.github.com>
2023-04-18 11:07:17 -0300

    autenticação com next auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,11 +1,13 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
-import { signIn, useSession } from 'next-auth/client';
+import { signIn, useSession, signOut } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
     const [session] = useSession();
+
+    console.log('minha seção', session);
 
     return (
         <>
@@ -14,11 +16,12 @@ export function SignInButton() {
                 <button
                     type="button"
                     className={styles.signInButton}
+                    onClick={() => signOut()}
                 >
                     <FaGithub
                         color='#04b361'
                     />
-                    Everton Carvalho
+                    {session.user.name}
                     <FiX
                         color='#737380'
                         className={styles.closeIcon}
